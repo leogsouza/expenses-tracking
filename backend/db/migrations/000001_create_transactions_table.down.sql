f2ca35b0d5fc88679bed54873c11e6c811dc19a1
Leonardo Souza <leogsouza@gmail.com>
2020-05-29 19:07:55 -0300

    update migration for initial tables

--- backend/db/migrations/000001_create_transactions_table.down.sql
+++ backend/db/migrations/000001_create_transactions_table.down.sql
@@ -1,3 +1,6 @@
 DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS accounts;
 DROP TABLE IF EXISTS users;
+DROP DOMAIN pk;
+DROP TYPE transaction_type;
+DROP TYPE transaction_status;
--- backend/db/migrations/000001_create_transactions_table.up.sql
+++ backend/db/migrations/000001_create_transactions_table.up.sql
@@ -1,33 +1,34 @@
-CREATE TYPE PK VARCHAR(32)
+CREATE DOMAIN pk AS 
+    VARCHAR(32) NOT NULL CHECK (value !~ '\s');
 
 CREATE TABLE IF NOT EXISTS users (
-  id PK PRIMARY KEY,
+  id pk PRIMARY KEY,
   name VARCHAR(45) NOT NULL,
   email VARCHAR(60) NOT NULL,
   password VARCHAR(45) NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()  
-)
+);
 
-INSERT INTO users (id, name, email, password, created_at) VALUES ("123456", "Leonardo", "teste@teste.com", "12345678", now());
+INSERT INTO users (id, name, email, password, created_at) VALUES ('123456', 'Leonardo', 'teste@teste.com', '12345678', now());
 
 CREATE TABLE IF NOT EXISTS accounts (
-  id PK PRIMARY KEY,
+  id pk PRIMARY KEY,
   name VARCHAR(45) NOT NULL,
   created_at TIMESTAMPTZ NOT NULL DEFAULT now()  
-)
+);
 
-INSERT INTO accounts (id, name, created_at) VALUES ("123456", "Carteira", now());
+INSERT INTO accounts (id, name, created_at) VALUES ('123456', 'Carteira', now());
 
-CREATE TYPE transaction_type AS ENUM ('Expense', 'Income')
-CREATE TYPE transaction_status AS ENUM('Pending', 'Outdated', 'Paid')
+CREATE TYPE transaction_type AS ENUM ('Expense', 'Income');
+CREATE TYPE transaction_status AS ENUM('Pending', 'Outdated', 'Paid');
 
 CREATE TABLE IF NOT EXISTS transactions (
-  id PK PRIMARY KEY,
-  user_id PK NOT NULL REFERENCES users,
+  id pk PRIMARY KEY,
+  user_id pk NOT NULL REFERENCES users,
   type transaction_type NOT NULL,  
-  account_id PK NOT NULL REFERENCES accounts
-  transaction_date TIMESTAMPTZ
-  status transaction_status NOT NULL DEFAULT 'Pending'
-  created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+  account_id pk NOT NULL REFERENCES accounts,
+  transaction_date TIMESTAMPTZ,
+  status transaction_status NOT NULL DEFAULT 'Pending',
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
   updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
-)
+);
