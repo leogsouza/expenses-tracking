2127cdf3e155dd7faf71fbd0b8828d9226776d57
Leonardo Souza <leogsouza@gmail.com>
2020-05-31 13:34:21 -0300

    omit user password on get user

--- backend/internal/entity/user.go
+++ backend/internal/entity/user.go
@@ -6,6 +6,6 @@ type User struct {
 	ID        ID        `json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 }
